d7df9ac8bbc4b04c4668119d255371310cc384ac
Bruno Alla <browniebroke@users.noreply.github.com>
2021-05-09 13:05:45 +0100

    Add script to purge runs older than 7 days

---
+++ purge-runs.sql
@@ -0,0 +1,22 @@
+SELECT COUNT(*) FROM builds;
+SELECT COUNT(*) FROM runs;
+SELECT COUNT(*) FROM statistics;
+
+WITH builds_to_delete AS (
+    SELECT id FROM "builds" WHERE "builds"."createdAt" < current_date - interval '7 days'
+),
+stats_to_delete AS (
+    DELETE FROM "statistics" WHERE "buildId" IN (
+        SELECT * FROM builds_to_delete
+    )
+    RETURNING id as deleted_stats_ids
+),
+runs_to_delete AS (
+    DELETE FROM "runs" WHERE "buildId" IN (
+        SELECT * FROM builds_to_delete
+    )
+    RETURNING id as deleted_runs_ids
+)
+DELETE FROM "builds" WHERE "builds"."id" in (
+    SELECT * FROM builds_to_delete
+);
